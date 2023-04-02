8f704d2320e711da203af94a31c1233632c2816e
alex <alex@ubuntu.localdomain>
2023-04-02 02:41:34 -0700

    Add initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,12 @@ pub mod gpio {
         PullDown, PullNone, PullUp,
     };
 }
+
+pub mod humidity {
+    use libtock_humidity as humidity;
+    pub type Humidity = humidity::Humidity<super::runtime::TockSyscalls>;
+}
+
 pub mod leds {
     use libtock_leds as leds;
     pub type Leds = leds::Leds<super::runtime::TockSyscalls>;
